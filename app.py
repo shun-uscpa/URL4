7e3de29b9c7466712a20ea5325c0f6da59c8fa32
shun-uscpa <49315514+shun-uscpa@users.noreply.github.com>
2021-07-10 22:16:25 +0900

    Add files via upload

---
+++ app.py
@@ -0,0 +1,33 @@
+import dash
+from dash.dependencies import Input, Output
+import dash_core_components as dcc
+import dash_html_components as html
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output
+
+from app import app
+from apps import app1, app2
+
+app = dash.Dash(__name__)
+server = app.server
+
+
+app.layout = html.Div([
+    dcc.Location(id='url', refresh=False),
+    html.Div(id='page-content')
+])
+
+
+@app.callback(Output('page-content', 'children'),
+              Input('url', 'pathname'))
+def display_page(pathname):
+    if pathname == '/apps/app1':
+        return app1.layout
+    elif pathname == '/apps/app2':
+        return app2.layout
+    else:
+        return '404'
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
